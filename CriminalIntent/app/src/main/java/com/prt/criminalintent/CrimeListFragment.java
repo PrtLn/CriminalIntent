3aa4ca5a4a79f7d11a4f7ece9298c941868d81a3
oleno <pirit.lena@gmail.com>
2017-02-10 15:55:29 +0200

    some corrections in the CrimeListFragment

--- CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeListFragment.java
@@ -1,6 +1,7 @@
 package com.prt.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 
 import java.util.ArrayList;
@@ -9,14 +10,7 @@ import java.util.ArrayList;
  * Created by L.Pikalova on 2/10/2017.
  */
 
-public class CrimeListFragment extends ListFragment {
+public class CrimeListFragment extends Fragment {
 
-    private ArrayList<Crime> mCrimes;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getActivity().setTitle(R.string.crimes_title);
-        mCrimes = (ArrayList<Crime>) CrimeLab.get(getActivity()).getCrimes();
-    }
 }
+
