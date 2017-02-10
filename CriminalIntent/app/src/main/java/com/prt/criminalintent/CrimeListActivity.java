2c0027f70dd500b9316a2e4c459b187379fb5b9f
oleno <pirit.lena@gmail.com>
2017-02-10 14:56:33 +0200

    an abstract activity for hosting a fragment (SingleFragmentActivity and CrimeListActivity new classes)

---
+++ CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeListActivity.java
@@ -0,0 +1,14 @@
+package com.prt.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by L.Pikalova on 2/10/2017.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
--- CriminalIntent/app/src/main/java/com/prt/criminalintent/SingleFragmentActivity.java
+++ CriminalIntent/app/src/main/java/com/prt/criminalintent/SingleFragmentActivity.java
@@ -1,12 +1,18 @@
 package com.prt.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
 
-public class CrimeActivity extends FragmentActivity {
-    /* called when the activity is first created */
+/**
+ * Created by L.Pikalova on 2/10/2017.
+ */
+
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +22,7 @@ public class CrimeActivity extends FragmentActivity {
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
 
         if (fragment == null) {
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragmentContainer, fragment)
                     .commit();
