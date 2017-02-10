5c00d85faa7fee99413cdcfac4dc425565b4ac22
oleno <pirit.lena@gmail.com>
2017-02-10 15:10:34 +0200

    clean up CrimeActivity and new controllers (CrimeListActivity and CrimelistFragment)

---
+++ CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeListFragment.java
@@ -0,0 +1,12 @@
+package com.prt.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by L.Pikalova on 2/10/2017.
+ */
+
+public class CrimeListFragment extends Fragment {
+
+
+}
