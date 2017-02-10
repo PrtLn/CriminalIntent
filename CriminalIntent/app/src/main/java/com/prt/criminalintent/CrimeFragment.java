407db6205408c2028bb1738744a9cf8eaf819184
oleno <pirit.lena@gmail.com>
2017-02-10 10:12:12 +0200

    overriding onCreateView method

--- CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeFragment.java
@@ -1,10 +1,29 @@
 package com.prt.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by L.Pikalova on 2/10/2017.
  */
 
 public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime, container, false);
+        return view;
+    }
 }
