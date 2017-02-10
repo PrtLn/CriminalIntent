ce15df710096067dea3950c809f59293f7569e30
oleno <pirit.lena@gmail.com>
2017-02-10 10:20:50 +0200

    wiring widgets in a fragment

--- CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeFragment.java
@@ -1,11 +1,13 @@
 package com.prt.criminalintent;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by L.Pikalova on 2/10/2017.
@@ -14,6 +16,7 @@ import android.view.ViewGroup;
 public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,24 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText)view.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //
+            }
+        });
         return view;
     }
 }
