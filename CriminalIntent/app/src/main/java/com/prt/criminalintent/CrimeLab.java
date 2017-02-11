1dee77fcd5dd2c5faf74a77ebb158046d198ebe7
oleno <pirit.lena@gmail.com>
2017-02-11 12:32:57 +0200

    using RecyclerView and some corrections

--- CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeLab.java
@@ -18,15 +18,15 @@ public class CrimeLab {
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
-            sCrimeLab = new CrimeLab(context.getApplicationContext());
+            sCrimeLab = new CrimeLab(context);
         }
         return sCrimeLab;
     }
 
-    private CrimeLab(Context appContext) {
-        mAppContext = appContext;
-        mCrimes = new ArrayList<Crime>();
-        for (int i = 0; i < 100; i++){
+    private CrimeLab(Context context) {
+        mAppContext = context;
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
             Crime crime = new Crime();
             crime.setTitle("Crime # " + i);
             crime.setSolved(i % 2 == 0); // every other one
