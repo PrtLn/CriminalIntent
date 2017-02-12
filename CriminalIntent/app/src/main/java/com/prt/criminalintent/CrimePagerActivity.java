9ed7b0cf12cfff74a5550ae468b2a3d5bd737594
oleno <pirit.lena@gmail.com>
2017-02-12 15:55:48 +0200

    creating CrimePagerActivity (using ViewPager)

--- CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimePagerActivity.java
@@ -1,8 +1,17 @@
 package com.prt.criminalintent;
 
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
 /**
- * Created by prt on 2/12/2017.
+ * Created by L.pikalova on 2/12/2017.
  */
 
-public class CrimePagerActivity {
+public class CrimePagerActivity extends FragmentActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
 }
