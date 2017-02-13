74d13b9b88108f0ce8d0549e5eb2d9fd7553f071
oleno <pirit.lena@gmail.com>
2017-02-13 10:39:27 +0200

    passing data between two fragments (passing data to DatePickerFragment)

--- CriminalIntent/app/src/main/java/com/prt/criminalintent/DatePickerFragment.java
+++ CriminalIntent/app/src/main/java/com/prt/criminalintent/DatePickerFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by L.Pikalova on 2/13/2017.
@@ -13,10 +17,36 @@ import android.view.View;
 
 public class DatePickerFragment extends DialogFragment
 {
+    private static final String ARG_DATE = "date";
+
+    private DatePicker mDatePicker;
+
+    // passing data to DatePickerFragment
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        // create a Calendar to get the year, month and day
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View view = LayoutInflater.from(getActivity())
                 .inflate(R.layout.dialog_date, null);
+
+        mDatePicker = (DatePicker) view.findViewById(R.id.dialog_gate_date_picker);
+        mDatePicker.init(year, month, day, null);
 
         return new AlertDialog.Builder(getActivity())
                 .setView(view)
