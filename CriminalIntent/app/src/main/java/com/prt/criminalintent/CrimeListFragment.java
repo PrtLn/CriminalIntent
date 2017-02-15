dde2190858423ec7b2457bd4c5679cf40c19bb56
oleno <pirit.lena@gmail.com>
2017-02-15 11:54:06 +0200

    displaying of a count of crimes on the toolbar (Show Subtitle)

--- CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeListFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateFormat;
@@ -69,9 +70,21 @@ public class CrimeListFragment extends Fragment
                         .newIntent(getActivity(), crime.getId());
                 startActivity(intent);
                 return true;
+            case R.id.menu_item_show_subtitle:
+                updateSubtitle();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void updateSubtitle() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        int crimeCount = crimeLab.getCrimes().size();
+        String subtitle = getString(R.string.subtitle_format, crimeCount);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.getSupportActionBar().setSubtitle(subtitle);
     }
 
     private void updateUI() {
