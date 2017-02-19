4df0fd555e5d6615722dca67550874261a59f84a
oleno <pirit.lena@gmail.com>
2017-02-19 13:02:46 +0200

    new widgets with 'include'(intents for work with the camera)

--- CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/prt/criminalintent/CrimeFragment.java
@@ -25,6 +25,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.Date;
 import java.util.UUID;
@@ -47,6 +49,8 @@ public class CrimeFragment extends Fragment {
     private CheckBox mSolvedCheckBox;
     private Button mReportButton;
     private Button mSuspectButton;
+    private ImageButton mPhotoButton;
+    private ImageView mPhotoView;
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -170,6 +174,9 @@ public class CrimeFragment extends Fragment {
                 PackageManager.MATCH_DEFAULT_ONLY) == null) {
             mSuspectButton.setEnabled(false);
         }
+
+        mPhotoButton = (ImageButton) view.findViewById(R.id.crime_camera);
+        mPhotoView = (ImageView) view.findViewById(R.id.crime_photo);
 
         return view;
     }
